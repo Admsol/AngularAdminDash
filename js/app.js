5b28259a4c380f762f05c4a65e9084f647e6a14d
Pierre <pierre@moneypush.com>
2015-11-30 14:54:49 +0000

    Token + status

--- js/app.js
+++ js/app.js
@@ -1,45 +1,51 @@
 var app = angular.module('myApp', []);
 
 app.controller('myController', ['$scope', 'MyLogin', function ($scope, MyLogin) {
-	var ref = new Firebase("https://glaring-heat-3574.firebaseio.com/");
-	var REST_back = "https://glaring-heat-3574.firebaseio.com/";
+    var ref = new Firebase("https://glaring-heat-3574.firebaseio.com/");
+    var REST_back = "https://glaring-heat-3574.firebaseio.com/";
+    var loginResult;
 
+    $scope.user = {
+        username: "",
+        pass: "",
+        token: "none",
+        status: "none"
+    };
 
-	$scope.user = {
-		username: "",
-		pass: ""
-	};
+    $scope.onLoginClick = function () {
+        loginResult = MyLogin.login($scope.user.username, $scope.user.pass).then(function (data) {
+            console.log(data);
 
-	console.log(this.user);
+            $scope.user.token = data.data.sessionToken;
+            $scope.user.status = data.statusText;
+        });
 
-
-	$scope.onLoginClick = function () {
-		MyLogin.login($scope.user.username, $scope.user.pass);
-	}
+    }
 
 }]);
 
 app.factory('MyLogin', ['$http', function ($http) {
-	return {
-		login: function (user, pass) {
-			$http({
-				url: "https://api.parse.com/1/login",
-				method: 'GET',
-				headers: {
-					'Content-Type': 'application/json',
-					"X-Parse-Application-Id": "AE7xZQU6JUDmrVuDKSHijRK00occMaAumpojNbSP",
-					"X-Parse-REST-API-Key": "TYD1uV2dQm0wZVgzC54NavGbRjPjqSlqaUBB98A9"
-				},
-				data: {
-					username: user,
-					password: pass
-				}
+    return {
+        login: function (user, pass) {
+            return $http({
+                url: "https://api.parse.com/1/users",
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    "X-Parse-Application-Id": "AE7xZQU6JUDmrVuDKSHijRK00occMaAumpojNbSP",
+                    "X-Parse-REST-API-Key": "TYD1uV2dQm0wZVgzC54NavGbRjPjqSlqaUBB98A9"
+                },
+                data: {
+                    username: user,
+                    password: pass
+                }
 
-			}).success(function (status) {
-				console.log(status);
-			}).error(function (status) {
-				console.log(status);
-			});
-		}
-	}
+            }).success(function (res) {
+                return res;
+            }).error(function (res) {
+                console.log("ERROR:" + res);
+            });
+            
+        }
+    }
 }]);
